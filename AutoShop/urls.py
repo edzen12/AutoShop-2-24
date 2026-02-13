56fb42a1b0a25ddf7cb03d6370c0c91d4cb8660e
edzen12 <oichiev.edzen@gmail.com>
2026-02-13 11:13:21 +0600

    django auto detail shop

---
+++ AutoShop/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('about/', include('apps.about.urls')),
+    path('blog/', include('apps.blog.urls')),
+    path('cart/', include('apps.cart.urls')),
+    path('contact/', include('apps.contact.urls')),
+    path('partners/', include('apps.partners.urls')),
+    path('product/', include('apps.product.urls')),
+    path('users/', include('apps.users.urls')),
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ README.md
@@ -0,0 +1,13 @@
+## Наш второй сайт на Django интернет магазин Auto Detail Shop
+
+### Инструкции по установке:
+### скачиваем этот проект через команду 
+#### git clone https://github.com/edzen12/AutoShop-2-24.git
+### открываем в VSCode, открываем терминал
+### и пишем команду 
+#### python -m venv venv
+#### .\venv\Scripts\activate
+#### pip install -r requirements.txt 
+#### python manage.py migrate
+#### python manage.py createsuperuser
+#### python manage.py runserver
