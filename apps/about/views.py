91105cc6899d2b489a46ef38af49c06857513527
edzen12 <oichiev.edzen@gmail.com>
2026-03-02 11:42:18 +0600

    view data in about page

--- apps/about/views.py
+++ apps/about/views.py
@@ -1,5 +1,17 @@
 from django.views.generic import TemplateView
+from apps.about.models import AboutContent, PlusAbout, BlogAbout, Faq, Testimonials
 
 
 class AboutView(TemplateView):
     template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['about'] = AboutContent.objects.latest('-id')
+        context['plusAbout'] = PlusAbout.objects.all()[:3]
+        context['blogAbout'] = BlogAbout.objects.all()[:3]
+        context['faq'] = Faq.objects.all()
+        context['testimonials'] = Testimonials.objects.all()[:5]
+        
+        return context
