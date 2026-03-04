7c9ed868d240813f63ad7bfc2687c4417594b85b
edzen12 <oichiev.edzen@gmail.com>
2026-03-04 11:58:38 +0600

    add contact html, and form

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,9 +1,19 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from apps.contact.models import ContactPage
+from apps.contact.forms import ContactRequestForm
 
 
-class ContactView(TemplateView):
+class ContactView(FormView):
     template_name = 'pages/contact.html'
+    success_url = reverse_lazy('contact')
+    form_class = ContactRequestForm
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['contactPage'] = ContactPage.objects.latest('-id')
         return context
