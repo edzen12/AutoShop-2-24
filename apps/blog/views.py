adb2238cd5c66c41fe10add3fd3edb56c5320419
edzen12 <oichiev.edzen@gmail.com>
2026-03-06 11:31:51 +0600

    blog

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,5 +1,18 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
+from apps.blog.models import Category, Tag, Post
 
 
-class BlogView(TemplateView):
+class BlogView(ListView):
+    model = Post
     template_name = 'pages/blog.html'
+    context_object_name = 'posts'
+    paginate_by = 6
+    ordering = ['-created_at']
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'pages/blog-detail.html'
+    context_object_name = 'post'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
