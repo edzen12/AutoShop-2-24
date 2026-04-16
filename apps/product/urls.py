4cc15983fa8be67bc79c03285f483bd0fce92090
edzen12 <oichiev.edzen@gmail.com>
2026-04-16 10:48:09 +0600

    edit header

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (
     CategoryListView, ProductListView,
     ProductDetailView, ReviewCreateView, HomeView, 
-    ToggleWishlistView, WishlistView
+    ToggleWishlistView, WishlistView, SearchView
 )
 
 urlpatterns = [
@@ -12,6 +12,7 @@ urlpatterns = [
     path('category/<slug:category_slug>/', ProductListView.as_view(), name='product_by_category'),
     path('<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
     path('<slug:slug>/review/', ReviewCreateView.as_view(), name='add_review'),
+    path('search/', SearchView.as_view(), name='search'),
     path('wishlist/', WishlistView.as_view(), name='wishlist'),
     path('wishlist/<int:product_id>', ToggleWishlistView.as_view(), name='wishlist_toggle'),
 ]
