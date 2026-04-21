b8e441c9db7796040d226fbdb74a4b19426398bc
edzen12 <oichiev.edzen@gmail.com>
2026-04-21 10:45:36 +0600

    auth part 1

--- apps/cart/utils.py
+++ apps/cart/utils.py
@@ -1,4 +1,4 @@
-from apps.cart.models import Cart
+from apps.cart.models import Cart, CartItem
 
 
 def get_cart(request):
@@ -7,5 +7,33 @@ def get_cart(request):
     else:
         if not request.session.session_key:
             request.session.create()
-        cart, _ = Cart.objects.get_or_create(session_key=request.session.session_key)
+        cart, _ = Cart.objects.get_or_create(
+            session_key = request.session.session_key
+        )
     return cart 
+
+
+def merge_cart(request, user):
+    session_key = request.session.session_key
+    if not session_key:
+        return
+    try:
+        session_cart = Cart.objects.get(session_key=session_key)
+    except Cart.DoesNotExist:
+        return
+    user_cart, _ = Cart.objects.get_or_create(user=user)
+
+    for item in session_cart.items.all():
+        existing_item = CartItem.objects.filter(
+            cart=user_cart,
+            product=item.product
+        ).first()
+
+        if existing_item:
+            existing_item.quantity += item.quantity
+            existing_item.save()
+        else:
+            item.cart = user_cart
+            item.save()
+
+    session_cart.delete()
